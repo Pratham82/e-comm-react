e5e914e9f5ce3ba34abf6688588d1e606427554b
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-03-29 17:58:23 +0530

    feat: added context providers and mockbee

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "components/navbar";
+import FilterProvider from "contexts/filter/filterState";
+import DataSourceProvider from "contexts/dataSource/dataSourceState";
 import Routing from "./routes";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <Routing />
+        <DataSourceProvider>
+          <FilterProvider>
+            <Navbar />
+            <Routing />
+          </FilterProvider>
+        </DataSourceProvider>
       </Router>
     </div>
   );
