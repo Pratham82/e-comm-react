307a5f571b9785374dfd3e9a36d0f4e3e453c157
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-03-29 17:57:41 +0530

    feat: updated pages

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,15 +1,22 @@
-// import "css/header.css";
 import "css/homepage.css";
 import img1 from "assets/images/homepage/balenciaga-white-track-low-top-sneakers-1.jpg";
 import img2 from "assets/images/homepage/balenciaga-track-faded-effect-sneakers-2.jpg";
 import img3 from "assets/images/homepage/balenciaga-grey-track-clear-sole-sneakers-3.jpg";
 import img4 from "assets/images/homepage/balenciaga-grey-triple-s-chunky-sneaker-4.jpg";
 import img5 from "assets/images/homepage/balenciaga-black-runner-sneakers-5.jpg";
+// import vid1 from "assets/videos/vid1.mp4";
 
 export default function Home() {
   const images = [img1, img2, img3, img4, img5];
   return (
     <main>
+      {/* <video loop autoPlay muted className="bg-video">
+        <source src={vid1} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video> */}
+      {/* <div className="bg-trs">
+        <div>Ts</div>
+      </div> */}
       {images.map((img) => (
         <div className="item" key={img}>
           <img src={img} alt="" />
--- src/pages/wishlist/index.tsx
+++ src/pages/wishlist/index.tsx
@@ -1,16 +1,16 @@
-import Product from "components/product";
+// import Product from "components/product";
 import "css/wishlist.css";
 
 export default function WishList() {
   return (
     <main>
       <section className="flex flex-wrap">
-        {Array(6)
+        {/* {Array(6)
           .fill("product")
           .map(() => (
             <Product wishList />
           ))}
-        <div className="card-new" />
+        <div className="card-new" /> */}
       </section>
     </main>
   );
