d2d8469ba5ddd1c0a4dd360b1b19966a27f654ae
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-04-01 21:15:21 +0530

    feat: added cart context in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "components/navbar";
 import FilterProvider from "contexts/filter/filterState";
 import DataSourceProvider from "contexts/dataSource/dataSourceState";
+import CartProvider from "contexts/cart/cartState";
 import Routing from "./routes";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <DataSourceProvider>
-          <FilterProvider>
-            <Navbar />
-            <Routing />
-          </FilterProvider>
-        </DataSourceProvider>
+        <CartProvider>
+          <DataSourceProvider>
+            <FilterProvider>
+              <Navbar />
+              <Routing />
+            </FilterProvider>
+          </DataSourceProvider>
+        </CartProvider>
       </Router>
     </div>
   );
