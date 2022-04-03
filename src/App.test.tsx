087ac9522c206e0dfdbcaa2b7b4dba0532f7cf28
Prathamesh Mali <mali.prathamesh82@gmail.com>
2022-04-03 16:28:06 +0530

    refactor: updated branding and testcase

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Min-Shop/i);
+  const linkElement = screen.getByText(/SNKR Town/i);
   expect(linkElement).toBeInTheDocument();
 });
